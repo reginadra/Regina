310763dc74ca6436b8fd672a4589e79c9ab5fd65
reginadra <100149430+reginadra@users.noreply.github.com>
2022-04-01 13:28:08 +0300

    Add files via upload
    homework 4

---
+++ string_overflow.c
@@ -0,0 +1,24 @@
+// homework 4
+
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    char s[40] = "Hello, ";
+    char name[20]; 
+	
+    puts("What is your name?");
+
+    // scanf %s -- string, %[^\n]s -- string with spaces (multiple words)
+    // Fix format string to also avoid overflow.
+    // See man 3 scanf or https://www.man7.org/linux/man-pages/man3/scanf.3.html#EXAMPLES
+	
+	// scanf("%[^\n]s", name);
+	fgets(name, sizeof(name), stdin); // Regina
+
+    strcat(s, name);
+    strcat(s, "!");
+    puts(s);
+    return 0;
+}
