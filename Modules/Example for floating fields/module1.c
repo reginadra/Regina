ce7af9322e0f9562458946ebcd408a4de761c7ab
reginadra <100149430+reginadra@users.noreply.github.com>
2022-05-12 18:02:02 +0300

    Add files via upload

---
+++ Modules/Example for floating fields/module1.c
@@ -0,0 +1,17 @@
+#include "module1.h"
+
+void print_bits(unsigned long long v, int width)
+{
+  char r[width+1];
+  r[width] = '\0';
+
+  for(int i = 0; i < width; r[i++] = '0');
+  while(width--)
+  {
+    if(v & 1)
+      r[width] = '1';
+    v >>= 1;
+  }
+  
+  printf("%s", r);
+}
---
+++ Modules/Example for floating fields/module1.h
@@ -0,0 +1,6 @@
+#ifndef _MODULE1_H
+#define _MODULE1_H	1
+
+void print_bits(unsigned long long, int);
+
+#endif
