a587019a8808a5fb4f14bafc4c795105c45035c4
reginadra <100149430+reginadra@users.noreply.github.com>
2022-05-20 16:04:38 +0300

    Add files via upload
    Another example for using modules in addition of a makefile

---
+++ Modules/Example for Hello World/00_helloworld.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "01_hello.h"
+
+void main()
+{
+	hello();
+}
---
+++ Modules/Example for Hello World/01_hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "01_hello.h"
+
+void hello(void)
+{
+	printf("hello, world\n");
+}
