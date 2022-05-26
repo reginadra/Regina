f8b7b0a08ef307dc12e2b9a1700a531745e8683e
reginadra <100149430+reginadra@users.noreply.github.com>
2022-05-26 23:47:08 +0300

    Add files via upload
    write and read binary files

---
+++ binary.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+
+struct Person
+{
+  char name[40];
+  int  age;
+};
+
+void read()
+{
+  struct Person ps[4];
+	
+  FILE *fi = fopen("1.bin", "rb");
+	
+  fread(ps, sizeof(struct Person), 4, fi);
+	
+  fclose(fi);
+}
+
+void write()
+{
+  struct Person ps[] = { {"Оля", 25}, {"Катя", 35} };
+
+  FILE *fo = fopen("1.bin", "wb");
+
+  fwrite(ps, sizeof(struct Person), 2, fo);
+  // fwrite(ps, sizeof(ps), 1, fo); // Isn't it enough to have one fwrite() function?
+
+  fclose(fo);
+}
+
+int main(int argc, char *argv[])
+{
+  switch(argv[1][0])
+  {
+    case 'w':
+      puts("w");
+      write();
+      break;
+    case 'r':
+      puts("r");
+      read();
+      break;
+    default:
+      puts("Error in command line");
+  }
+
+  return 0;
+}
